b52167b8f3b7596a457dcb8fbf00996a139ad0db
Vlad Pavlovski <vladislav.pavlovski@ad-in-one.com>
2020-04-16 22:08:11 +0200

    fix task page

--- src/components/Forms/Task.js
+++ src/components/Forms/Task.js
@@ -2,7 +2,7 @@ import Avatar from '@material-ui/core/Avatar'
 import ListItemText from '@material-ui/core/ListItemText'
 import MenuItem from '@material-ui/core/MenuItem'
 import PropTypes from 'prop-types'
-import React, { Component } from 'react'
+import React from 'react'
 import TextField from 'rmw-shell/lib/components/ReduxFormFields/TextField'
 import { Field, reduxForm, formValueSelector } from 'redux-form'
 import { VirtualizedSelectField } from 'muishift'
@@ -13,76 +13,74 @@ import { setDialogIsOpen } from 'rmw-shell/lib/store/dialogs/actions'
 import { withRouter } from 'react-router-dom'
 import { withTheme } from '@material-ui/core/styles'
 
-class Form extends Component {
-  render() {
-    const { handleSubmit, intl, initialized, users } = this.props
+const Form = props => {
+  const { handleSubmit, intl, initialized, users } = props
 
-    return (
-      <form
-        onSubmit={handleSubmit}
-        style={{
-          height: '100%',
-          width: '100%',
-          alignItems: 'strech',
-          display: 'flex',
-          flexWrap: 'wrap',
-          justifyContent: 'center',
-        }}
-      >
-        <button type="submit" style={{ display: 'none' }} />
+  return (
+    <form
+      onSubmit={handleSubmit}
+      style={{
+        height: '100%',
+        width: '100%',
+        alignItems: 'strech',
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+      }}
+    >
+      <button type="submit" style={{ display: 'none' }} />
+
+      <div>
+        <div>
+          <Field
+            name="title"
+            disabled={!initialized}
+            component={TextField}
+            placeholder={intl.formatMessage({ id: 'title_hint' })}
+            label={intl.formatMessage({ id: 'title_label' })}
+          />
+        </div>
 
         <div>
-          <div>
-            <Field
-              name="title"
-              disabled={!initialized}
-              component={TextField}
-              placeholder={intl.formatMessage({ id: 'title_hint' })}
-              label={intl.formatMessage({ id: 'title_label' })}
-            />
-          </div>
-
-          <div>
-            <Field
-              name="description"
-              disabled={!initialized}
-              component={TextField}
-              multiline
-              placeholder={intl.formatMessage({ id: 'description_hint' })}
-              label={intl.formatMessage({ id: 'description_label' })}
-            />
-          </div>
-
-          <br />
-          <div>
-            <Field
-              name="helper"
-              rowHeight={54}
-              component={VirtualizedSelectField}
-              items={users.map(snap => (snap && snap.val ? snap.val : {}))}
-              itemToString={item => (item ? item.displayName : '')}
-              inputProps={{
-                placeholder: intl.formatMessage({ id: 'helper_hint' }),
-                label: intl.formatMessage({ id: 'helper_label' }),
-              }}
-              renderSuggestion={({ downshiftProps, suggestion, index }) => {
-                const { getItemProps, highlightedIndex } = downshiftProps
-                const itemProps = getItemProps({ item: suggestion })
-                const isHighlighted = highlightedIndex === index
-
-                return (
-                  <MenuItem {...itemProps} selected={isHighlighted} key={index}>
-                    <Avatar alt="avatar" src={suggestion.photoURL} />
-                    <ListItemText primary={suggestion.displayName} />
-                  </MenuItem>
-                )
-              }}
-            />
-          </div>
+          <Field
+            name="description"
+            disabled={!initialized}
+            component={TextField}
+            multiline
+            placeholder={intl.formatMessage({ id: 'description_hint' })}
+            label={intl.formatMessage({ id: 'description_label' })}
+          />
         </div>
-      </form>
-    )
-  }
+
+        <br />
+        <div>
+          <Field
+            name="helper"
+            rowHeight={54}
+            component={VirtualizedSelectField}
+            items={users.map(snap => (snap && snap.val ? snap.val : {}))}
+            itemToString={item => (item ? item.displayName : '')}
+            inputProps={{
+              placeholder: intl.formatMessage({ id: 'helper_hint' }),
+              label: intl.formatMessage({ id: 'helper_label' }),
+            }}
+            renderSuggestion={({ downshiftProps, suggestion, index }) => {
+              const { getItemProps, highlightedIndex } = downshiftProps
+              const itemProps = getItemProps({ item: suggestion })
+              const isHighlighted = highlightedIndex === index
+
+              return (
+                <MenuItem {...itemProps} selected={isHighlighted} key={index}>
+                  <Avatar alt="avatar" src={suggestion.photoURL} />
+                  <ListItemText primary={suggestion.displayName} />
+                </MenuItem>
+              )
+            }}
+          />
+        </div>
+      </div>
+    </form>
+  )
 }
 
 Form.propTypes = {
--- src/pages/Tasks/Tasks.js
+++ src/pages/Tasks/Tasks.js
@@ -6,46 +6,48 @@ import CollectionActivity from 'rmw-shell/lib/containers/Activities/CollectionAc
 import Divider from '@material-ui/core/Divider'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
-import React, { Component } from 'react'
+import React, { useCallback } from 'react'
 import { compose } from 'redux'
 import { injectIntl } from 'react-intl'
 import { withRouter } from 'react-router-dom'
 import { withTheme } from '@material-ui/core/styles'
 
-class Tasks extends Component {
-  renderItem = (key, val) => {
-    const { history } = this.props
+const Tasks = props => {
+  const { history } = props
+  const renderItem = useCallback(
+    (key, val) => {
+      const { title = '', full_name = '', photoURL } = val
+      return (
+        <div key={key}>
+          <ListItem
+            onClick={() => history.push(`/tasks/edit/${key}`)}
+            key={key}
+          >
+            <AltIconAvatar alt="task" src={photoURL} icon={<Delete />} />
+            <ListItemText
+              primary={title}
+              secondary={full_name}
+              style={{ minWidth: 120 }}
+            />
+          </ListItem>
+          <Divider variant="inset" />
+        </div>
+      )
+    },
+    [history]
+  )
 
-    const { title = '', full_name = '', photoURL } = val
+  const filterFields = [{ name: 'name' }, { name: 'full_name' }]
 
-    return (
-      <div key={key}>
-        <ListItem onClick={() => history.push(`/tasks/edit/${key}`)} key={key}>
-          <AltIconAvatar alt="task" src={photoURL} icon={<Delete />} />
-          <ListItemText
-            primary={title}
-            secondary={full_name}
-            style={{ minWidth: 120 }}
-          />
-        </ListItem>
-        <Divider variant="inset" />
-      </div>
-    )
-  }
-
-  render() {
-    const filterFields = [{ name: 'name' }, { name: 'full_name' }]
-
-    return (
-      <CollectionActivity
-        name="tasks"
-        createGrant="create_task"
-        filterFields={filterFields}
-        renderItem={this.renderItem}
-        isGranted={s => g => true}
-      />
-    )
-  }
+  return (
+    <CollectionActivity
+      name="tasks"
+      createGrant="create_task"
+      filterFields={filterFields}
+      renderItem={renderItem}
+      isGranted={s => g => true}
+    />
+  )
 }
 
 Tasks.propTypes = {
