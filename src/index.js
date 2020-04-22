50a4774eede7d1001f2c92c8b343232f5be25848
Vlad Pavlovski <vladislav.pavlovski@ad-in-one.com>
2020-04-22 21:40:56 +0200

    project cleanup

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import * as serviceWorker from 'rmw-shell/lib/utils/serviceWorker'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Loadable from 'react-loadable'
-import LoadingComponent from 'rmw-shell/lib/components/LoadingComponent'
+import LoadingComponent from './components/LoadingComponent'
 import 'react-perfect-scrollbar/dist/css/styles.css'
 //import App from './App'
 
